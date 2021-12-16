cce2425e4de378edee4d5db1c0b4832644a903e4
ChannarongTangmuang <96216863+ChannarongTangmuang@users.noreply.github.com>
2021-12-16 11:46:11 +0700

    ADD C to F

---
+++ TempCalculator/TempCalculator/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace TempCalculator
+{
+    partial class buttonConvert
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.textBoxC = new System.Windows.Forms.TextBox();
+            this.textBoxF = new System.Windows.Forms.TextBox();
+            this.Form1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(220, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(83, 31);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Convert";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(177, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "C";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(178, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(15, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "F";
+            // 
+            // textBoxC
+            // 
+            this.textBoxC.Location = new System.Drawing.Point(20, 23);
+            this.textBoxC.Name = "textBoxC";
+            this.textBoxC.Size = new System.Drawing.Size(151, 22);
+            this.textBoxC.TabIndex = 4;
+            this.textBoxC.Text = "100";
+            this.textBoxC.TextChanged += new System.EventHandler(this.textBoxC_TextChanged);
+            // 
+            // textBoxF
+            // 
+            this.textBoxF.Location = new System.Drawing.Point(20, 68);
+            this.textBoxF.Name = "textBoxF";
+            this.textBoxF.Size = new System.Drawing.Size(151, 22);
+            this.textBoxF.TabIndex = 5;
+            this.textBoxF.TextChanged += new System.EventHandler(this.textBoxF_TextChanged);
+            // 
+            // Form1
+            // 
+            this.Form1.Location = new System.Drawing.Point(220, 64);
+            this.Form1.Name = "Form1";
+            this.Form1.Size = new System.Drawing.Size(82, 31);
+            this.Form1.TabIndex = 6;
+            this.Form1.Text = "Convert";
+            this.Form1.UseVisualStyleBackColor = true;
+            this.Form1.Click += new System.EventHandler(this.Form1_Click);
+            // 
+            // buttonConvert
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(416, 184);
+            this.Controls.Add(this.Form1);
+            this.Controls.Add(this.textBoxF);
+            this.Controls.Add(this.textBoxC);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "buttonConvert";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.TextBox textBoxC;
+        private System.Windows.Forms.TextBox textBoxF;
+        private System.Windows.Forms.Button Form1;
+    }
+}
+
---
+++ TempCalculator/TempCalculator/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TempCalculator
+{
+    public partial class buttonConvert : Form
+    {
+        public buttonConvert()
+        {
+            InitializeComponent();
+        }
+
+        private void textBoxF_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // get input from textbox
+            string input = textBoxC.Text;
+            // f= c * 9/5 + 32
+            double c = Convert.ToDouble(input);
+            double f = c * 9 / 5 + 32;
+           
+            // show input to textbox
+            textBoxF.Text = f.ToString();
+        }
+
+        private void textBoxC_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            string input = textBoxF.Text;
+            double f = Convert.ToDouble(input);
+            double c = (f - 32) * 5 / 9;
+            textBoxC.Text = c.ToString();
+        }
+    }
+}
